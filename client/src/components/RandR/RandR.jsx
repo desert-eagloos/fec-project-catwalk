e7d0c7f90b45ff4e28efac714f9d4997c9837252
Glenmore Vinoya <glenmore@pinoyak.com>
2021-02-27 16:35:45 -0900

    refactored css and jsx codes for separation of concerns

--- client/src/components/RandR/RandR.jsx
+++ client/src/components/RandR/RandR.jsx
@@ -1,29 +1,17 @@
 import React from 'react';
 import '../../css/RandR/RandR.css';
 
+import Ratings from './Ratings';
+import Reviews from './Reviews';
+
 const RandR = () => (
   <div className="module-randr-container">
     <div className="randr-title">
       <h2>Ratings and Reviews</h2>
     </div>
     <div className="randr-items">
-      <div className="randr-ratings-container">
-        <div>Avg</div>
-        <div>Stars</div>
-        <div>Star Breakdown</div>
-        <div>Size</div>
-        <div>Comfort</div>
-      </div>
-      <div className="randr-reviews-container">
-        <div>Total reviews and sorting</div>
-        <div>
-          <div>star rating</div>
-          <div>user and date posted</div>
-        </div>
-        <div>review title</div>
-        <div>review body</div>
-        <div>helpful and report</div>
-      </div>
+      <Ratings />
+      <Reviews />
     </div>
   </div>
 );
