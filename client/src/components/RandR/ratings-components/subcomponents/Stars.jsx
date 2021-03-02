366b205319adb86eca6acf9c5394816a6dd68a45
Glenmore Vinoya <glenmore@pinoyak.com>
2021-03-02 11:28:01 -0900

    removed @fortawesome fontawesome packages

--- client/src/components/RandR/ratings-components/subcomponents/Stars.jsx
+++ client/src/components/RandR/ratings-components/subcomponents/Stars.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import Rating from 'react-rating';
 
 /* --- Import Fontawesome library --- */
 import fontawesome from '@fortawesome/fontawesome';
@@ -10,63 +11,74 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 /* --- add icons to our fontawesome library --- */
 fontawesome.library.add(solid, regular);
 
-export default class Stars extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rating: props.rating,
-    };
-  }
-
-  fullStars() {
-    const { rating } = this.state;
-    return Math.floor(rating);
-  }
-
-  halfStars() {
-    const { rating } = this.state;
-    const x = rating % 1;
-    return x >= 0.5 ? 1 : 0;
-  }
-
-  emptyStars() {
-    return 5 - this.fullStars() - this.halfStars();
-  }
-
-  render() {
-    const fullStars = this.fullStars();
-    const halfStars = this.halfStars();
-    const emptyStars = this.emptyStars();
-
-    const renderFullStars = () => (fullStars !== 0
-      ? Array(fullStars)
-        .fill(null)
-        .map((item, i) => <FontAwesomeIcon className="star" key={`fs${i}`} icon="star" />)
-      : '');
-
-    const renderHalfStars = () => (halfStars !== 0
-      ? Array(halfStars)
-        .fill(null)
-        .map((item, i) => (
-          <span key={`hs${i}`} className="fa-layers fa-fw star">
-            <FontAwesomeIcon icon="star-half" />
-            <FontAwesomeIcon icon={['far', 'star-half']} flip="horizontal" />
-          </span>
-        ))
-      : '');
-
-    const renderEmptyStars = () => (emptyStars !== 0
-      ? Array(emptyStars)
-        .fill(null)
-        .map((item, i) => (<FontAwesomeIcon className="star" key={`es${i}`} icon={['far', 'star']} />))
-      : '');
-
-    return (
-      <div className="star-rating">
-        {renderFullStars()}
-        {renderHalfStars()}
-        {renderEmptyStars()}
-      </div>
-    );
-  }
+export default function Stars() {
+  return (
+    <div>
+      <Rating
+        readonly
+        emptySymbol="fa fa-star-0"
+        fullSymbol="fa fa-star"
+      />
+    </div>
+  );
 }
+// export default class Stars extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       rating: props.rating,
+//     };
+//   }
+
+//   fullStars() {
+//     const { rating } = this.state;
+//     return Math.floor(rating);
+//   }
+
+//   halfStars() {
+//     const { rating } = this.state;
+//     const x = rating % 1;
+//     return x >= 0.5 ? 1 : 0;
+//   }
+
+//   emptyStars() {
+//     return 5 - this.fullStars() - this.halfStars();
+//   }
+
+//   render() {
+//     const fullStars = this.fullStars();
+//     const halfStars = this.halfStars();
+//     const emptyStars = this.emptyStars();
+
+//     const renderFullStars = () => (fullStars !== 0
+//       ? Array(fullStars)
+//         .fill(null)
+//         .map((item, i) => <FontAwesomeIcon className="star" key={`fs${i}`} icon="star" />)
+//       : '');
+
+//     const renderHalfStars = () => (halfStars !== 0
+//       ? Array(halfStars)
+//         .fill(null)
+//         .map((item, i) => (
+//           <span key={`hs${i}`} className="fa-layers fa-fw star">
+//             <FontAwesomeIcon icon="star-half" />
+//             <FontAwesomeIcon icon={['far', 'star-half']} flip="horizontal" />
+//           </span>
+//         ))
+//       : '');
+
+//     const renderEmptyStars = () => (emptyStars !== 0
+//       ? Array(emptyStars)
+//         .fill(null)
+//         .map((item, i) => (<FontAwesomeIcon className="star" key={`es${i}`} icon={['far', 'star']} />))
+//       : '');
+
+//     return (
+//       <div className="star-rating">
+//         {renderFullStars()}
+//         {renderHalfStars()}
+//         {renderEmptyStars()}
+//       </div>
+//     );
+//   }
+// }
