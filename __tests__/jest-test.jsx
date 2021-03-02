1a31287d43703b1e312c29e1a94fc125d79760ae
Glenmore Vinoya <glenmore@pinoyak.com>
2021-03-02 12:25:29 -0900

    completed star rating component using npm package react-rating added stub component for rating breakdown added test on jest for creating stub rating breakdown component added test on jest for creating star rating component

--- __tests__/jest-test.jsx
+++ __tests__/jest-test.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import App from '../client/src/components/App.jsx';
+
+import App from '../client/src/components/App';
 import BootstrapTest from '../client/bootstrapTests/BootstrapTest';
+import Stars from '../client/src/components/RandR/ratings-components/subcomponents/Stars';
+import RatingsBreakdown from '../client/src/components/RandR/ratings-components/subcomponents/RatingsBreakdown';
 
 describe('Welcome (Snapshot)', () => {
   it('Welcome renders App.jsx', () => {
@@ -16,5 +19,22 @@ describe('Creates a React-Bootsrap Component', () => {
     const component = renderer.create(<BootstrapTest />);
     const json = component.toJSON();
     expect(json).toMatchSnapshot();
-  })
+  });
+});
+
+describe('Creates Ratings and Reviews Components', () => {
+  describe('Creates a Star Rating Component', () => {
+    it('Creates a Star', () => {
+      const component = renderer.create(<Stars />);
+      const json = component.toJSON();
+      expect(json).toMatchSnapshot();
+    });
+  });
+  describe('Creates a Rating Breakdown Component', () => {
+    it('Creates a Rating Breakdown', () => {
+      const component = renderer.create(<RatingsBreakdown />);
+      const json = component.toJSON();
+      expect(json).toMatchSnapshot();
+    });
+  });
 });
