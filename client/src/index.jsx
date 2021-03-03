cdfe5e8daec357a7d6db598304762b6343340832
Glenmore Vinoya <69649257+kuyavinny@users.noreply.github.com>
2021-03-03 12:15:28 -0900

    remove tailwind css
    Accidentally left tailwind css.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,5 @@
-import axios from 'axios';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-
-import './tailwind.output.css';
 
 render(<App />, document.getElementById('app'));
