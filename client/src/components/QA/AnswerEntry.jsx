d7a459b71bad58b532c9f86b3c94845391138f06
Jake Gray <jakegray1717@gmail.com>
2021-03-02 18:36:59 -0700

    I created the accordion feature so that only four questions and up to 2 answers per questions will render by default. If there are more than two answers and more than 4 questions there is a button to see more and show less questions and answers.

--- client/src/components/QA/AnswerEntry.jsx
+++ client/src/components/QA/AnswerEntry.jsx
@@ -2,11 +2,20 @@ import React, { useState } from 'react';
 
 const AnswerEntry = (props) => {
 
-  return (
-    <div>
-      A:{props.answer}
-    </div>
-  )
+  if (props.answer) {
+    return (
+      <div>
+        A:{props.answer}
+        <div>
+        by [username], Month DD, YYYY
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div></div>
+    )
+  }
 }
 
 export default AnswerEntry;
--- client/src/components/QA/AnswerList.jsx
+++ client/src/components/QA/AnswerList.jsx
@@ -1,15 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AnswerEntry from './AnswerEntry';
 
 const AnswerList = (props) => {
 
+  const allAnswers = props.answers;
+
+  const firstTwoAns = [props.answers[0], props.answers[1]];
+
+  const [answers, setAnswers] = useState(firstTwoAns);
+
+  const [open, setOpen] = useState(false);
+
+  const [moreButton, setMoreButton] = useState('More Answers');
+
+  const toggleAnswers = () => {
+    setOpen(!open);
+  }
+
+  const renderMoreAnswersButton = () => {
+    if (allAnswers.length > 2) {
+      return (
+        (<button onClick={() => toggleAnswers()}>{moreButton}</button>)
+      )
+    }
+  }
+
+  useEffect(() => {
+      if (open) {
+      setAnswers(props.answers);
+      setMoreButton('Show Less');
+    } else {
+      setAnswers(firstTwoAns);
+      setMoreButton('More Answers');
+    }
+  }, [open]);
+
   return (
     <div>
-      {props.answers.map((answer, index) => {
-        return (
-          <AnswerEntry answer={answer} key={index}/>
-        )
-      })}
+      {
+        answers.map((answer, index) => {
+
+          return (
+            <AnswerEntry answer={answer} key={index}/>
+          )
+        })
+      }
+      {renderMoreAnswersButton()}
     </div>
   )
 }
