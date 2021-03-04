e30f45abcea719350c082dce592bdda7da912fdd
Glenmore Vinoya <glenmore@pinoyak.com>
2021-03-04 13:29:16 -0900

    staged tests for size fit rating

--- __tests__/RandR/RandR.spec.jsx
+++ __tests__/RandR/RandR.spec.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import expect from 'expect';
+import { describe, test } from 'jest';
+
 import Stars from '../../client/src/components/RandR/ratings-components/subcomponents/Stars';
 import RatingsBreakdown from '../../client/src/components/RandR/ratings-components/subcomponents/RatingsBreakdown';
 
 describe('Creates Ratings and Reviews Components', () => {
   describe('Creates a Star Rating Component', () => {
-    test('Creates a Star', () => {
+    test('Star', () => {
       const component = renderer.create(<Stars />);
       const json = component.toJSON();
       expect(json).toMatchSnapshot();
     });
   });
   describe('Creates a Rating Breakdown Component', () => {
-    test('Creates a Rating Breakdown', () => {
+    test(' Rating Breakdown', () => {
+      const ratings = {
+        5: 2,
+        4: 3,
+        3: 5,
+        2: 1,
+        1: 10,
+      };
+      const component = renderer.create(<RatingsBreakdown key="bcTest1" ratings={ratings} />);
+      const json = component.toJSON();
+      expect(json).toMatchSnapshot();
+    });
+  });
+  describe('Creates a Size Fit Rating Component', () => {
+    test('Size Fit Rating', () => {
       const ratings = {
         5: 2,
         4: 3,
